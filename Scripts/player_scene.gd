56813670866c9cd12afc6a0b8ece7420560c28e4
denzio231 <108673932+denzio231@users.noreply.github.com>
2025-06-03 17:32:08 +0800

    global_transform

--- Scripts/player_scene.gd
+++ Scripts/player_scene.gd
@@ -32,12 +32,11 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	var input_dir := Input.get_vector("LEFT","RIGHT","UP","DOWN")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
